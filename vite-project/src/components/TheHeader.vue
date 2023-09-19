c7a8a1b63d7a74ca9ac08941f287a32562f4067a
Toyonari Deushi <zombieship@gmail.com>
2023-09-19 10:23:47 +0900

    P50までの内容

---
+++ vite-project/src/components/TheHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <TheHeader text="My Counter" />
+  <MyComponent v-bind:text="value" />
+</template>
+
+<script>
+import TheHeader from "./TheHeader.vue";
+
+export default {
+  name: "App",
+  components: {
+    TheHeader,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
